5c40f3fb4673c38c7115ee72c5ef5537de0698b9
Rayhan <nofussdufus@gmail.com>
2022-01-10 11:13:49 +0000

    Start tutorials

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,10 @@
+
+function TodoContainer() {
+  return (
+    <div className="TodoContainer">
+     <h1>To Do List</h1>
+    </div>
+  );
+}
+
+export default TodoContainer;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,10 @@
+
+function TodoItem(props) {
+  return (
+    <div className="TodoItem">
+     
+    </div>
+  );
+}
+
+export default TodoItem;
